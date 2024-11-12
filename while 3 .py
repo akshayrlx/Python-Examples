3950de51472d60cb8f1d4890922a312d1bace918
akshayrlx <akshayrlx4@gmail.com>
2024-11-12 14:13:58 +0530

    Add files via upload

---
+++ while 3 .py
@@ -0,0 +1,45 @@
+list2=[]
+d=input("enter a value and 100 foer quit ")
+while d!='100':
+    sum(d)
+    d=input("enter a value and 100 for quit")
+
+f=int(input("enter a value and sum"))
+sum=0
+while f!=0:
+    sum=sum+f
+    print(sum)
+    f=int(input("enter a value and sum"))
+
+z=int(input("enter a value 0 for quit"))
+while z<10:
+    print(z)
+    z=int(input("enter a value 0 for quit"))
+
+z=int(input("enter a value 0 for quit"))
+while z>10:
+    print(z)
+    z=int(input("enter a value 0 for quit"))
+
+list1=[]
+x=int(input("enter a value "))
+while x<10:
+    list1.append(x)
+    x=int(input("enter a value"))
+list1
+
+list1=[]
+x=int(input("enter a value "))
+while x<10:
+    list1.append(x)
+    print(x)
+    x=int(input("enter a value"))
+list1
+
+
+a=int(input("enter a value and sum"))
+sum=0
+while a<7:
+    sum=sum+a
+    print(sum)
+    a=int(input("enter a value and sum"))
