205b15633ee0e7d9eab3df69de6e7bcb7a761fb8
akshayrlx <akshayrlx4@gmail.com>
2024-11-14 14:11:09 +0530

    Add files via upload

---
+++ while 4.py
@@ -0,0 +1,19 @@
+
+marks=[24,45,567,89,8,9,4,5]
+index = 0
+while index <8:
+    print(marks[index]) 
+    index=index+1
+
+marks=[24,45,567,89,8,9,4,5]
+index = 0
+while index <8:
+    if marks [index]==8:
+        break
+    print(marks[index]) 
+    index=index+1
+    
+index = 0
+while marks[index]!=8:
+    print(marks[index]) 
+    index=index+1
