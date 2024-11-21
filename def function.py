c6044c0d252756551ab07912cf892e0918dc717d
akshayrlx <akshayrlx4@gmail.com>
2024-11-21 13:57:48 +0530

    Add files via upload

--- def function.py
+++ def function.py
@@ -6,10 +6,18 @@ def akshay():
             print(" * ",end=" ")
         print(" * ")
 akshay()
+def akshay(a=5):
+    for raw in range(0,a):
+        for space in range (raw,a):
+            print(" ",end=" ")
+        for star in range (0,raw):
+            print(" * ",end=" ")
+        print(" * ")
+akshay(8)
 
  
 
-def akshay1(d):
+def akshay1(d=5):
     
     for row in range(1,d):
         for star in range(1,d):
@@ -19,5 +27,26 @@ def akshay1(d):
                print(star,end="")
         
         print(" ")
-akshay1(10)
+akshay1()
+
+
+def pattern():
+    a=int(input("enter a value and sum"))
+    sum=0
+    while a<7:
+       sum=sum+a
+       print(sum)
+       a=int(input("enter a value and sum"))
+pattern()
+
+def values():
+    list1=[]
+    x=int(input("enter a value "))
+    while x<10:
+        list1.append(x)
+        print(x)
+        x=int(input("enter a value"))
+    print(list1)
+values()
+
 
