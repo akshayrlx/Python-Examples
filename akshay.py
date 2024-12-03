9586cc51e3375325f9978858c8b78dacbc8804d4
akshayrlx <akshayrlx4@gmail.com>
2024-12-03 14:10:22 +0530

    Add files via upload

---
+++ akshay.py
@@ -0,0 +1,76 @@
+1# 
+list1=[10,20,30,40,50,60,70,80,90]
+square=1
+for i in list1:
+    square=square*i
+    print(square) 
+
+2#
+list2=[10,20,30,40,50,60,70,80,90]
+index=list2[0]
+for i in list2:
+    if i>index:
+        index=i
+print(index)  
+
+3#
+index=list2[0]
+for i in list2:
+    if i<index:
+        index=i
+print(index)   
+   
+4#
+
+list3=["abba","accc","daa","OCCCO","AAA","aa"]
+count=0
+for i in list3:
+    if len(i)>=2:
+        if i[0]==i[-1]:
+            count=count+1
+print(count)
+
+5#
+
+list4=[100,200,300,400,100,200,700,100]
+list5=[]
+for i in list4:
+    if i not in list5:
+        list5.append(i)
+print(list5)
+
+6#
+list1=[1]
+if len(list1)>0:
+     print("the list is not empty")
+else:
+    print("the list is empty")
+
+7#
+list10=["apple","ball","cat","dog","elephant"]
+n=4
+for i in list10:
+    if len(i)>n:
+       print(i)
+
+8#       
+list7= ["red","green","white","black","pink","yellow"]
+count = 0
+list10=[]
+for i in range(0, len(list7)):
+    if i not in [0,4,5]:
+        list10.append(list7[i])
+print(list10)  
+
+9#
+
+p=int(input("enter number:"))
+def prime(a):
+    if a>1:
+        for i in range (2,a):
+            if a%i==0:
+                return False
+            else:
+                return True
+prime(p)
+
